80432d3b34ab127fcededd5dc38893f1f85e1793
kleber0815 <78033720+kleber0815@users.noreply.github.com>
2025-12-07 14:12:56 +0100

    Update Supabase client import URL
    Changed the Supabase client import from jsdelivr to esm.sh with a specific version for improved reliability and version control after recent Downtimes.

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,5 +1,5 @@
 // Supabase configuration
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.3'
 
 const supabaseUrl = 'https://uombtmlxjqgzqkpedcgn.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbWJ0bWx4anFnenFrcGVkY2duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMTY5MzksImV4cCI6MjA2NDg5MjkzOX0.KkfKVCm7KCj3v2kTW4TpqY0J0Rb7tuetZyuGcRxkdS8'
